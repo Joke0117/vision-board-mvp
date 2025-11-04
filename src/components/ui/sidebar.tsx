ec12b82aa598d1c7d51dc8ffaddccef09a89a8e6
jose angel <martinezrodelojose@gmail.com>
2025-11-04 17:51:36 -0500

    Descripción

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -153,16 +153,16 @@ const Sidebar = React.forwardRef<
     return (
       <Sheet open={open} onOpenChange={setOpen} {...props}>
         <SheetContent
-          data-sidebar="sidebar"
-          data-mobile="true"
-          className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
-          style={
-            {
-              "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
-            } as React.CSSProperties
-          }
-          side={side}
-        >
+  data-sidebar="sidebar"
+  data-mobile="true"
+  className="w-[--sidebar-width] bg-background p-0 text-foreground [&>button]:hidden"
+  style={
+    {
+      "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
+    } as React.CSSProperties
+  }
+  side={side}
+>
           <div className="flex h-full w-full flex-col">{children}</div>
         </SheetContent>
       </Sheet>
